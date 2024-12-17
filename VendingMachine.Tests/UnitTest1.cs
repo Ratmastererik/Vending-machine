8d421de2638590447b1b79a3807fb32773412f10
Erky <erik.lindgrenw@gmail.com>
2024-12-17 11:52:01 +0100

    .

--- VendingMachine.Tests/UnitTest1.cs
+++ VendingMachine.Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace VendingMachine.Tests;
 
 public class UnitTest1
@@ -8,12 +10,12 @@ public class UnitTest1
     {
         // Arrange
         int coinValue;
-        List<string> coins = ["Quarter", "Dime", "Nickle", "Penny"];
-        int expected = 40;
+        string coin = "Quarter";
+        int expected = 25;
         VendingMachine newVendingmachine = new();
 
         // Act
-        coinValue = newVendingmachine.identifyCoins(coins);
+        coinValue = newVendingmachine.identifyCoins(coin);
 
         // Assert
         Assert.Equal(expected, coinValue);
@@ -23,10 +25,10 @@ public class UnitTest1
     public void RejectPennysTest()
     {
         // Arrange
-        int coinValue;
-        List<string> penny = ["Penny"];
-        int expected = 0;
         VendingMachine newVendingmachine = new();
+        string penny = "Penny";
+        int coinValue;
+        int expected = 0;
 
         // Act
         coinValue = newVendingmachine.identifyCoins(penny);
@@ -34,43 +36,126 @@ public class UnitTest1
         // Assert
         Assert.Equal(expected, coinValue);
     }
+
     [Fact]
     public void Dispalay_INSERTCOIN_WhenBalanceIsZero()
     {
         // Arrange
         VendingMachine testMachine = new();
         string expected = "INSERT COIN";
+        
         // Act
         
         // Assert
         Assert.Equal(testMachine.onDisplay, expected);
     }
-    [Theory]
-    [InlineData(0)]
-    [InlineData(1)]
-    [InlineData(2)]
-    public void ProductIsSelected(int productID)
-    {
-        {
-            VendingMachine testMachine = new();
-            // Arrange
-            string actual = testMachine.SelectProduct(productID);
 
-            // Act
-
-            // Assert
-            Assert.Contains(actual, testMachine.products);
-        }
-    }
     [Fact]
-    public void InsertNickleWillChange_totalAmountTo()
+    public void ThankYouMessageTest()
     {
         // Arrange
         VendingMachine testMachine = new();
+        string expected = "THANK YOU";
+
         // Act
-        testMachine.insertCoin("Nicle");
+        string actual = testMachine.ThankYouMessage();
 
         // Assert
-        Assert.True(testMachine.totalAmount == 5);
+        Assert.Equal(actual, expected);
+    }
+
+    [Theory]
+    [InlineData(110, 110, "INSERT COIN")]
+    [InlineData(100, 100, "INSERT COIN")]
+    [InlineData(70, 70, "INSERT COIN")]
+    public void CancelReturnCoinsTest(int money, int expectedChange, string expectedDisplayMessage)
+    {
+        {
+            // Arrange
+            VendingMachine testMachine = new();
+            testMachine.InsertedCoins = money;
+
+            // Act
+            int actualChange = testMachine.Cancel();
+            string actualDispalyMessage = testMachine.onDisplay;
+
+            // Assert
+            Assert.Equal(expectedChange, actualChange );
+            Assert.Equal(expectedDisplayMessage, actualDispalyMessage);
+        }
+    }
+
+    [Theory]
+    [InlineData(0, 110, 10)]
+    [InlineData(1, 100, 50)]
+    [InlineData(2, 70, 5)]
+    public void ReturnChangeTest(int productID, int money, int expectedChange)
+    {
+        {
+            // Arrange
+            VendingMachine testMachine = new();
+            testMachine.SelectProduct(productID, money);
+
+            // Act
+            int actualChange = testMachine.InsertedCoins;
+
+            // Assert
+            Assert.Equal(expectedChange, actualChange);
+        }
+    }
+
+    [Theory]
+    [InlineData(0, 100)]
+    [InlineData(1, 50)]
+    [InlineData(2, 65)]
+    public void SuccesfulProductSelect(int productID, int money)
+    {
+        {
+            // Arrange
+            VendingMachine testMachine = new();
+            string [] expectedItem = ["Cola", "Chips", "Candy"];
+
+            // Act
+            string actualItem = testMachine.SelectProduct(productID, money);
+
+            // Assert
+            Assert.Contains(expectedItem[productID], actualItem);
+        }
+    }
+    
+    [Theory]
+    [InlineData(0, 50)]
+    [InlineData(1, 50)]
+    [InlineData(2, 65)]
+    public void FailedProductSelect(int productID, int money)
+    {
+        {
+            // Arrange
+            VendingMachine testMachine = new();
+            string [] expectedItem = ["INSERT COIN", "Chips", "Candy"];
+
+            // Act
+            string actualItem = testMachine.SelectProduct(productID, money);
+
+            // Assert
+            Assert.Contains(expectedItem[productID], actualItem);
+        }
+    }
+
+    [Theory]
+    [InlineData(0, 200, "SOLD OUT")]
+    public void ProductsStockTest(int productID, int money, string expected )
+    {
+        {
+            // Arrange
+            VendingMachine testMachine = new();
+
+            // Act
+            testMachine.SelectProduct(productID, money);
+            string actual = testMachine.SelectProduct(productID, money);
+
+            // Assert
+            Assert.Contains(expected, actual);
+        }
     }
 }
---
+++ VendingMachine/VendingMachine.cs
@@ -0,0 +1,91 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace VendingMachine;
+
+public class VendingMachine
+{
+    public string onDisplay { get; set; }
+    public string[] Products = ["Cola", "Chips", "Candy"];
+    public int[] ProductsStock = [1, 5, 1000];
+    public int[] prize = [100, 50, 65];
+    public int InsertedCoins { get; set; }
+    int Nickle = 5;
+    int Dime = 10;
+    int Quarter = 25;
+
+    public VendingMachine()
+    {
+        onDisplay = "INSERT COIN";
+    }
+
+    public int identifyCoins(string coinType)
+    {
+            switch (coinType)
+            {
+                case "Nickle":
+                    InsertedCoins += Nickle;
+                    break;
+                case "Dime":
+                    InsertedCoins += Dime;
+                    break;
+                case "Quarter":
+                    InsertedCoins += Quarter;
+                    break;
+                default:
+                    break;
+            }
+        return InsertedCoins;
+    }
+
+    public string SelectProduct(int productID, int money)
+    {   
+        InsertedCoins = money;
+        if (InsertedCoins >= prize[productID] && ProductsStock[productID] > 0)
+        {
+            Console.WriteLine(ThankYouMessage());
+            InsertedCoins -= prize[productID];
+            ProductsStock[productID] -= 1;
+            ReturnChange(InsertedCoins);
+            return Products[productID];
+        }
+        else if (ProductsStock[productID] == 0)
+        {
+            return "SOLD OUT";
+        }
+        else
+        {   
+            return onDisplay;
+        }
+    }
+
+    public int Cancel()
+    {   
+        return InsertedCoins;
+    }
+
+    public void CheckDisplay()
+    {
+        Console.WriteLine(onDisplay);
+    }
+
+    // public bool CheckStock(int productID)
+    // {
+    //     if (ProductsStock[productID] == 0)
+    //     {
+    //         onDisplay = "SOLD OUT";
+    //     }
+    //     return onDisplay;
+    // }
+
+    public string ThankYouMessage()
+    {
+        onDisplay = "THANK YOU";
+        return onDisplay;
+    }
+
+    public int ReturnChange(int change)
+    {
+        return change;
+    }
+}
